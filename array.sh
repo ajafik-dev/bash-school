b2db1fedcf5606b53a2897a7d3eece49cf1edb75
Oluwafikayo Ajayi <o.oluwafikayoajayi@gmail.com>
2025-10-01 22:01:05 +0100

    removed the commas in array

--- array.sh
+++ array.sh
@@ -2,7 +2,7 @@
 
 # BASIC ARRAY OPERATIONS
 
-items=("Item 1", "Item 2", "Item 3", "Item 4") # creates the array
+items=("Item 1" "Item 2" "Item 3" "Item 4") # creates the array
 
 echo ${items[1]} # Output second element of the array
 echo ${items[2]} # Output the 3rd element of the array
