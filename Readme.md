e459362f773d879218208c2367799b65a02bdd53
Oluwafikayo Ajayi <o.oluwafikayoajayi@gmail.com>
2025-10-01 21:22:09 +0100

    variables, sum, arguments

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
 # Bash School
+
+- chmod +x hello.sh => chmod +x <filename>.sh => Make the file executable then run ./hello.sh OR bash hello.sh
+
+bash hello.sh Sade is a good boy
---
+++ hello.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Hello World!"
+touch ade.txt
+
+SURNAME="Oluwafikayo"
+echo "My name is $SURNAME Ajayi"
+
+echo $SURNAME
+echo ${SURNAME}
+
+FIRST_NO=10
+SECOND_NO=20
+SUM=$((FIRST_NO + SECOND_NO))
+echo "The sum of $FIRST_NO and $SECOND_NO is $SUM"
+
+
+echo "[Gets the first text before space] Welcome " $1
+
+echo "All input with space as one" $@
+
+
