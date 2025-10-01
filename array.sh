b9add1a739bb5f37ae2f4cec9645faf020a05ac8
Oluwafikayo Ajayi <o.oluwafikayoajayi@gmail.com>
2025-10-01 22:18:49 +0100

    array slicing operations

--- array.sh
+++ array.sh
@@ -9,3 +9,13 @@ echo ${items[2]} # Output the 3rd element of the array
 echo ${items[@]} # Output all the elements of the array
 
 echo ${#items[@]} # outputs the length of the array
+
+
+# ARRAY SLICING
+
+array=("A" "B" "C" "D" "E")
+echo "${array[@]}" # Prints the entire array elements
+echo "${array[1]}" # Access the second element of the array
+echo "${array[@]:1:3}" # Starts from position 1 copy 3 elements
+echo "${array[@]:1}" # Starts from the index to the end
+
